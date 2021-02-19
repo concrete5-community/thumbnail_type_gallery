eafec6eabfc0461a1d3327be8a1b03255e5a666f
a3020 <a3020@fastmail.com>
2021-02-19 18:28:52 +0100

    Version 1.0

--- controller.php
+++ controller.php
@@ -12,7 +12,7 @@ class Controller extends Package
 {
     protected $pkgHandle = 'thumbnail_type_gallery';
     protected $appVersionRequired = '8.0.0';
-    protected $pkgVersion = '0.9.1';
+    protected $pkgVersion = '1.0';
     protected $pkgAutoloaderRegistries = [
         'src/ThumbnailTypeGallery' => '\A3020\ThumbnailTypeGallery',
     ];
